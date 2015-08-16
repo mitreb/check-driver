f9f3447701dea4dbb831cb85d4050d80462da95f
otimgit <otimworkmail@gmail.com>
2015-08-16 10:37:51 +0300

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+var file_url = process.argv[2],
+    utils = require('utils');
+
+if (/^https?:/.test(file_url)) {
+  utils.downloadFile(file_url, function (file) {
+    processDriver(file['path']);
+  });
+} else if (/\.zip$/.test(file_url)) {
+  processDriver(file_url);
+} else {
+  utils.readFile(file_url, checkINFFile);
+}
+
+function processDriver(file_name) {
+  var dir_name;
+  utils.unzipFile(file_name);
+  dir_name =  file_name.replace(/\.zip$/, '')
+  utils.findFile(dir_name, /\.inf$/, function (err, inf_file) {
+     if (err) {
+       console.error(err);
+     } else {
+       utils.readFile(inf_file, checkINFFile);
+     }
+  });
+}
+
+function checkINFFile(data) {
+  var os_versions, output;
+  os_versions = utils.parseOS(data.toString());
+  if (os_versions) {
+    output = utils.formatOutput(os_versions);
+    console.log(output)
+  } else {
+    console.error('OS versions are not found in the INF file');
+  }
+}
